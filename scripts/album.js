224a560f79fe0981f80ce9597caa12e37b55312c
Allen Kramer <kramer.alleng@gmail.com>
2016-07-15 13:19:44 -0400

    removed songListContainer and songRows variables, removed click event listener from .ready()

--- scripts/album.js
+++ scripts/album.js
@@ -116,39 +116,6 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-var findParentByClassName = function(element, targetClass) {
-    if (element) {
-        if (element.parentElement === null) {
-            alert("No parent found");
-        } else {
-            var currentParent = element.parentElement;
-            while (currentParent.className != targetClass && currentParent.className !== null) {
-                currentParent = currentParent.parentElement;
-            }
-
-            return currentParent;
-        }
-    }
-};
-
-var getSongItem = function(element) {
-    switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-            return findParentByClassName(element, 'song-item-number');
-        case 'album-view-song-item':
-            return element.querySelector('.song-item-number');
-        case 'song-item-title':
-        case 'song-item-duration':
-            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-        case 'song-item-number':
-            return element;
-        default:
-            return;
-    }
-};
-
 var clickHandler = function(targetElement) {
     var songItem = getSongItem(targetElement);
 
@@ -166,10 +133,6 @@ var clickHandler = function(targetElement) {
      }
 };
 
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-
-var songRows = document.getElementsByClassName('album-view-song-item');
-
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
@@ -177,10 +140,4 @@ var currentlyPlayingSong = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumRunDMC);
-
-    for(var i = 0; i < songRows.length; i++) {
-        songRows[i].addEventListener('click', function(event) {
-            clickHandler(event.target);
-        });
-    }
 });
